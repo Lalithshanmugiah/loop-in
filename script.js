207e2dff5535921dab45a91d4e9810cf7dddf1dd
Lalithshanmugiah <89589081+Lalithshanmugiah@users.noreply.github.com>
2023-08-21 16:08:30 +0530

    new

--- script.js
+++ script.js
@@ -3,4 +3,18 @@ let text ='';
 for(let x in person){
     text += person[x] +" " + "<br>"
 }
-document.getElementById("para").innerHTML=text 
+document.getElementById("para").innerHTML=text;
+
+
+
+
+//for each
+
+const numbers =[20,30,40,50];
+let text1 ="";
+numbers.forEach(myFunction);
+document.getElementById("para").innerHTML=text;
+function myFunction(value, index, array){
+    text += value + "<br>"
+}
+
